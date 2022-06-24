8b7c36df15f93361d4a203d2b61d29cbd437a567
Dragan2402 <63748840+Dragan2402@users.noreply.github.com>
2022-06-24 21:45:50 +0200

    Update output.asm

--- mini-c-klase/output.asm
+++ mini-c-klase/output.asm
@@ -1,21 +1,15 @@
 
-@funkcija1Test_body:
-		MOV 	p,%13
-		JMP 	@funkcija1Test_exit
-@funkcija1Test_exit:
-		MOV 	%14,%15
-		POP 	%14
-		RET
 main:
 		PUSH	%14
 		MOV 	%15,%14
 		SUBS	%15,$8,%15
 @main_body:
-		MOV 	$5,-4(%14)
-			CALL	funkcija1Test
-		MOV 	%13,-8(%14)
-			CALL	funkcija1Test
-		MOV 	%13,%13
+		MOV 	$11,-8(%14)
+		MOV 	$13,-4(%14)
+		ADDS	-4(%14),$1,%0
+		MOV 	%0,-4(%14)
+		ADDS	-8(%14),-4(%14),%0
+		MOV 	%0,%13
 		JMP 	@main_exit
 @main_exit:
 		MOV 	%14,%15
